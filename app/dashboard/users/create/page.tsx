5839e4ec1570f344892b66f58045cd5daac8e32f
sfwnisme <92028514+sfwnisme@users.noreply.github.com>
2025-12-09 02:58:51 +0300

    refactor: remove async syntax

--- app/dashboard/users/create/page.tsx
+++ app/dashboard/users/create/page.tsx
@@ -1,7 +1,6 @@
 import CreateUserFormView from '@/features/users/views/mutation-views/create-user-form-view';
-import React from 'react'
 
-export default async function page() {
+export default function page() {
   return (
     <div>
       <CreateUserFormView />

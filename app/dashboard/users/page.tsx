3a1114404e9f3c8152d821cd73b805a2bd3d0951
sfwnisme <92028514+sfwnisme@users.noreply.github.com>
2025-12-09 16:14:51 +0300

    feat: show create user button by permission function

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,8 +1,10 @@
 import { Button } from "@/components/ui/button";
 import { PAGES_ROUTES } from "@/constants/config";
+import can from "@/features/dashboard/auth/can";
 import UsersTableSkeleton from "@/features/users/skeletons/users-table-skeleton";
 import UsersTableView from "@/features/users/views/users-table-view";
 import { SearchParamsType } from "@/types/types";
+import { Visible } from "@sfwnisme/visi";
 import Link from "next/link";
 import React, { Suspense } from "react";
 type Props = {
@@ -10,20 +12,24 @@ type Props = {
 };
 const { CREATE } = PAGES_ROUTES.USERS;
 export default async function page({ searchParams }: Props) {
+  const canCreateUser = await can("user.write");
+
   const searchParamsRes = await searchParams;
   return (
     <div className="w-full">
-    <div className="inline-flex justify-between w-full mb-12">
-      <h1 className="scroll-m-20 text-center text-4xl font-medium tracking-tight text-balance">
-        Users
-      </h1>
-      <Button asChild>
-        <Link href={CREATE}>Create user</Link>
-      </Button>
-    </div>
-    <Suspense fallback={<UsersTableSkeleton count={10} />}>
-      <UsersTableView searchParams={searchParamsRes} />
-    </Suspense>
+      <div className="inline-flex justify-between w-full mb-12">
+        <h1 className="scroll-m-20 text-center text-4xl font-medium tracking-tight text-balance">
+          Users
+        </h1>
+        <Visible when={canCreateUser}>
+          <Button asChild>
+            <Link href={CREATE}>Create user</Link>
+          </Button>
+        </Visible>
+      </div>
+      <Suspense fallback={<UsersTableSkeleton count={10} />}>
+        <UsersTableView searchParams={searchParamsRes} />
+      </Suspense>
     </div>
   );
 }

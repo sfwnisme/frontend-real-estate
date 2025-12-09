9a586b9ebfce57b338fd1661dfe8ae4b82813dab
sfwnisme <92028514+sfwnisme@users.noreply.github.com>
2025-12-09 03:05:21 +0300

    feat: apply permissions to UI and functions
    define permissions to links
    
    filter routes depends on permission

--- features/dashboard/dashboard-sidebar.tsx
+++ features/dashboard/dashboard-sidebar.tsx
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 
-import * as React from "react"
+import * as React from "react";
 import {
   Book,
   BookPlus,
@@ -8,22 +8,44 @@ import {
   House,
   HousePlus,
   LibraryBig,
+  LucideIcon,
   NotebookPen,
-} from "lucide-react"
+  User,
+  UserPlus,
+  Users,
+} from "lucide-react";
 
-import { DashboardNavRoutes } from "./dashboard-nav-routes"
-import { DashboardNavUser } from "./dashboard-nav-user"
+import { DashboardNavRoutes } from "./dashboard-nav-routes";
+import { DashboardNavUser } from "./dashboard-nav-user";
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
   SidebarHeader,
   SidebarRail,
-} from "@/components/ui/sidebar"
-import Image from "next/image"
+} from "@/components/ui/sidebar";
+import Image from "next/image";
+import useCan from "./auth/use-can";
+import { Permission } from "@/constants/permissions";
+
+type FilteredRoute = {
+  name: string;
+  url: string;
+  icon: LucideIcon;
+  permission: Permission;
+};
+type user = {
+  name: string;
+  email: string;
+  avatar: string;
+};
 
 // This is sample data.
-const data = {
+
+const data: {
+  user: user;
+  routes: FilteredRoute[];
+} = {
   user: {
     name: "safwan",
     email: "safwanmabdo@gmail.com",
@@ -34,55 +56,80 @@ const data = {
       name: "Overview",
       url: "/dashboard",
       icon: Frame,
+      permission: "property.read",
     },
     {
       name: "Properties",
       url: "/dashboard/properties",
       icon: House,
+      permission: "property.read",
     },
     {
       name: "Create New Property",
       url: "/dashboard/properties/create",
       icon: HousePlus,
+      permission: "property.write",
     },
     {
       name: "Blog Posts",
       url: "/dashboard/blog-posts",
       icon: LibraryBig,
+      permission: "blogpost.read",
     },
     {
       name: "Create Blog Posts",
       url: "/dashboard/blog-posts/create",
       icon: NotebookPen,
+      permission: "blogpost.write",
     },
     {
       name: "Users",
       url: "/dashboard/users",
-      icon: LibraryBig,
+      icon: Users,
+      permission: "user.read",
     },
     {
       name: "Create Users",
       url: "/dashboard/users/create",
-      icon: NotebookPen,
+      icon: UserPlus,
+      permission: "user.write",
     },
   ],
-}
+};
 
-export function DashboardSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+
+export function DashboardSidebar({
+  ...props
+}: React.ComponentProps<typeof Sidebar>) {
+
+  const {can} = useCan()
+  
+  const filteredRoutes = data.routes.filter((route) => {
+    const canSee = can(route.permission);
+    if (!canSee) return null;
+    return route;
+  });
+  
   return (
     <Sidebar collapsible="icon" {...props}>
       <SidebarHeader>
         <div className="w-full p-2">
-          <Image src='/logoipsum.png' height={300} width={300} alt="project logo" className="w-full"/>
+          <Image
+            src="/logoipsum.png"
+            height={300}
+            width={300}
+            alt="project logo"
+            className="w-full"
+          />
         </div>
       </SidebarHeader>
       <SidebarContent>
-        <DashboardNavRoutes projects={data.routes} />
+        <DashboardNavRoutes projects={filteredRoutes} />
       </SidebarContent>
       <SidebarFooter>
         <DashboardNavUser user={data.user} />
       </SidebarFooter>
       <SidebarRail />
     </Sidebar>
-  )
+  );
 }

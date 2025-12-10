01d9a593fd268f18df1deda217dde89177a0d1b0
sfwnisme <92028514+sfwnisme@users.noreply.github.com>
2025-12-10 22:05:52 +0300

    feat: save locale option on cookies

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,8 +1,10 @@
 import { getRequestConfig } from "next-intl/server";
+import { cookies } from "next/headers";
 
 export default getRequestConfig(async () => {
   // Static form now, we'll change this later
-  const locale = "en";
+  const cookieStore = await cookies()
+  const locale = cookieStore.get("locale")?.value || "en"
 
   return {
     locale,

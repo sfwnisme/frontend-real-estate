6e67e7e6b8bacd2a9d117d0b7e18fcf841b13bc5
sfwnisme <92028514+sfwnisme@users.noreply.github.com>
2025-12-11 00:32:34 +0300

    feat: language navigator link

--- features/client/nav-view.tsx
+++ features/client/nav-view.tsx
@@ -1,13 +1,18 @@
+"use client"
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '../../components/ui/dropdown-menu'
 import { MenuIcon } from 'lucide-react'
 import { PAGES_ROUTES } from '@/constants/config'
+import { Link as NextIntlLink } from "@/i18n/navigation"
+import { getLocale } from 'next-intl/server'
+import { useLocale } from 'next-intl'
 
 type Props = {}
 
 export default function Nav({ }: Props) {
+
   return (
     <nav className='flex items-center h-fit px-6 py-6 sticky top-0 z-50 bg-red- backdrop-blur-xs'>
       <div className='w-50 max-w-full h-auto overflow-hidden me-auto'>
@@ -20,6 +25,7 @@ export default function Nav({ }: Props) {
         <li><Link href={PAGES_ROUTES.ABOUT.PREVIEW} className='font-medium'>About</Link></li>
         <li><Link href={PAGES_ROUTES.CONTACT.PREVIEW} className='font-medium'>Contact</Link></li>
         <li><Link href={PAGES_ROUTES.BLOG_POSTS.PREVIEW} className='font-medium'>Blog</Link></li>
+        <li><NextIntlLink href="/" locale={useLocale() === "ar"? "en" : "ar"} className='font-medium'>{useLocale() === "ar" ? "English" : "العربية"}</NextIntlLink></li>
       </ul>
       <DropdownMenu>
         <DropdownMenuTrigger className='max-md:block hidden'>

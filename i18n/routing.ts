df3149e8a6030c1fd7935fbec6bbc4f448e2c0a3
sfwnisme <92028514+sfwnisme@users.noreply.github.com>
2025-12-19 14:58:45 +0300

    docs: comment important instructions

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -3,5 +3,8 @@ import { defineRouting } from "next-intl/routing";
 export const routing = defineRouting({
   locales: ["en", "ar"],
   defaultLocale: "ar",
+  // "always" -> /locale
+  // "as-needed" -> defaultLocale = /, en = /en
+  // "never" -> all = /
   localePrefix: "as-needed",
 });

3d84c4382cfbbc8c1fdb994411e1695d7cedfeb0
sfwnisme <92028514+sfwnisme@users.noreply.github.com>
2025-12-19 11:22:51 +0300

    refactor: remove unused code

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,9 +1,6 @@
 import { hasLocale } from "next-intl";
 import { getRequestConfig } from "next-intl/server";
-import { cookies } from "next/headers";
 import { routing } from "@/i18n/routing";
-
-
 
 export default getRequestConfig(async ({requestLocale}) => {
   const requested = await requestLocale;

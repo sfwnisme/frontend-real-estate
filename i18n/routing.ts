7b740d9a2db687982a8a2a8922e58dd4e48e21b5
sfwnisme <92028514+sfwnisme@users.noreply.github.com>
2025-12-19 15:14:55 +0300

    feat: disable language detector
    - allow URL matcher, for users that use the URLs

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -7,4 +7,7 @@ export const routing = defineRouting({
   // "as-needed" -> defaultLocale = /, en = /en
   // "never" -> all = /
   localePrefix: "as-needed",
+  // true -> rely on browser accept-language header request.
+  // false -> relys on user's cookies, also enble URL matching.
+  localeDetection: false,
 });

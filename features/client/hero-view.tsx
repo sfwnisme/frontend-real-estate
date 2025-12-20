15c269887285a0d10598823c111a29eeebc4e298
sfwnisme <92028514+sfwnisme@users.noreply.github.com>
2025-12-20 23:05:47 +0300

    feat: translate hero section

--- features/client/hero-view.tsx
+++ features/client/hero-view.tsx
@@ -6,10 +6,13 @@ import {
   AvatarImage,
 } from "../../components/ui/avatar";
 import { Button } from "../../components/ui/button";
+import { useTranslations } from "next-intl";
 
 type Props = {};
 
 export default function Hero({}: Props) {
+  const t = useTranslations("HomePage")
+  console.log(t("title"))
   return (
       <header
         className="relative h-[calc(100dvh-100px)] box-border bg-center bg-cover bg-no-repeat bg-gray-100 rounded-4xl"
@@ -20,28 +23,11 @@ export default function Hero({}: Props) {
           size="lg"
           className="absolute bg-transparent backdrop-blur-sm bottom-4 left-1/2 -translate-x-1/2 text-white"
         >
-          Explore homes <ArrowDown />
+          {t("button")} <ArrowDown />
         </Button>
         <div className="flex flex-col items-center gap-4 bg-gray-100 rounded-3xl py-8 px-6 absolute md:bottom-4 md:top-auto top-1/2 md:right-4 right-1/2 md:translate-y-0 -translate-y-1/2 md:translate-x-0 translate-x-1/2 text-center">
           <h1 className="text-7xl font-medium">70+</h1>
-          <p className="text-sm w-40">Experts working on your success</p>
-          <div className="*:data-[slot=avatar]:ring-background flex -space-x-2 *:data-[slot=avatar]:ring-2 *:data-[slot=avatar]:grayscale">
-            <Avatar>
-              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-            <Avatar>
-              <AvatarImage src="https://github.com/leerob.png" alt="@leerob" />
-              <AvatarFallback>LR</AvatarFallback>
-            </Avatar>
-            <Avatar>
-              <AvatarImage
-                src="https://github.com/evilrabbit.png"
-                alt="@evilrabbit"
-              />
-              <AvatarFallback>ER</AvatarFallback>
-            </Avatar>
-          </div>
+          <p className="text-sm w-40">{t("description")}</p>
         </div>
       </header>
   );

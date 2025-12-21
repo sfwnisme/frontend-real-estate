a1c9c2eb992486fedd2e275b3a43755b73558cb4
sfwnisme <92028514+sfwnisme@users.noreply.github.com>
2025-12-21 22:21:40 +0300

    fix: title width

--- features/client/faq-view.tsx
+++ features/client/faq-view.tsx
@@ -1,35 +1,55 @@
-import React from 'react'
-import Title from '../../components/custom/title'
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '../../components/ui/accordion'
-import { ChevronDown, ChevronDownCircle } from 'lucide-react'
-import { faqsDummyData } from '@/data/dummyData'
-import { getTranslations } from 'next-intl/server'
+import React from "react";
+import Title from "../../components/custom/title";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "../../components/ui/accordion";
+import { ChevronDown, ChevronDownCircle } from "lucide-react";
+import { faqsDummyData } from "@/data/dummyData";
+import { getTranslations } from "next-intl/server";
 
-type Props = {}
+type Props = {};
 
-export default async function Faq({ }: Props) {
-  const t = await getTranslations("HomePage.FaqSection")
-  console.log("questions" ,t.raw("Questions"))
+export default async function Faq({}: Props) {
+  const t = await getTranslations("HomePage.FaqSection");
+  console.log("questions", t.raw("Questions"));
 
   return (
-    <div className='flex max-lg:flex-col items-start gap-8 lg:gap-16 justify-between'>
-      <Title type='start' title={t("title")} description={t("description")} />
-      <div className='w-full'>
+    <div className="flex max-lg:flex-col items-start gap-8 lg:gap-16 justify-between">
+      <div className="lg:min-w-[400px] w-full flex-1">
+        <Title type="start" title={t("title")} description={t("description")} />
+      </div>
+      <div className="w-full">
         <Accordion
           type="single"
           collapsible
           className="w-full"
           defaultValue="item-1"
         >
-          {t.raw("Questions").map((faq: { question: string, answer: string }) => (
-            <AccordionItem value={String(faq.question)} key={faq.question} className='border-gray-100'>
-              <AccordionTrigger>{faq.question}<ChevronDown strokeWidth="1" className='border border-gray-200 rounded-full ' /></AccordionTrigger>
-              <AccordionContent className="flex flex-col gap-4 text-balance">
-                <p>{faq.answer}</p>
-              </AccordionContent>
-            </AccordionItem>
-          ))}
-        </Accordion></div>
+          {t
+            .raw("Questions")
+            .map((faq: { question: string; answer: string }) => (
+              <AccordionItem
+                value={String(faq.question)}
+                key={faq.question}
+                className="border-gray-100"
+              >
+                <AccordionTrigger>
+                  {faq.question}
+                  <ChevronDown
+                    strokeWidth="1"
+                    className="border border-gray-200 rounded-full "
+                  />
+                </AccordionTrigger>
+                <AccordionContent className="flex flex-col gap-4 text-balance">
+                  <p>{faq.answer}</p>
+                </AccordionContent>
+              </AccordionItem>
+            ))}
+        </Accordion>
+      </div>
     </div>
-  )
+  );
 }

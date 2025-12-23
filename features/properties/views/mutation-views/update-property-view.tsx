4eba2f010c387ff51534e42a00feaedacdd2dee7
sfwnisme <92028514+sfwnisme@users.noreply.github.com>
2025-12-24 01:21:03 +0300

    feat: translate update property view component

--- features/properties/views/mutation-views/update-property-view.tsx
+++ features/properties/views/mutation-views/update-property-view.tsx
@@ -2,6 +2,7 @@
 
 import { ImageType, Property } from "@/types/types";
 import React, { useMemo } from "react";
+import { useTranslations } from "next-intl";
 import UpdatePropertyFormView from "./update-property-form-view";
 import CreateNewPropertyImagesFormView from "./create-new-property-images-form-view";
 import FieldSet from "@/components/custom/field-set";
@@ -15,6 +16,8 @@ type Props = {
 
 export default function UpdatePropertyView(props: Props) {
   const { property, images } = props;
+  const tSections = useTranslations("common.form.sections");
+  const tDescs = useTranslations("common.form.descriptions");
 
   // Stable property reference - only updates when key fields change
   const stableProperty = useMemo(
@@ -34,18 +37,18 @@ export default function UpdatePropertyView(props: Props) {
     <div className="flex flex-col lg:flex-row gap-4 relative">
       <UpdatePropertyFormView property={stableProperty} />
       <div className="sticky top-0 flex flex-col gap-4 lg:basis-1/3">
-        <FieldSet title="Slug" description="The slug is the URL of the property">
+        <FieldSet title={tSections("slug")} description={tDescs("slugDescription")}>
           <UpdatePropertySlugFormView property={stableProperty} />
         </FieldSet>
         <FieldSet
-          title="Current images"
-          description="View the current images for the property"
+          title={tSections("currentImages")}
+          description={tDescs("currentImagesDescription")}
         >
           <UploadedPropertyImagesView images={stableImages} />
         </FieldSet>
         <FieldSet
-          title="Upload images"
-          description="Upload new images for the property"
+          title={tSections("uploadImages")}
+          description={tDescs("uploadImagesDescription")}
         >
           <CreateNewPropertyImagesFormView
             propertyId={property._id}

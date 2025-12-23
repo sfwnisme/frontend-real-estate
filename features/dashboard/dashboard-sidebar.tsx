f6595696c07b9a585fbabd2686ea6aba3e89a8ce
sfwnisme <92028514+sfwnisme@users.noreply.github.com>
2025-12-24 00:13:25 +0300

    feat: configure sidebar translation

--- features/dashboard/dashboard-sidebar.tsx
+++ features/dashboard/dashboard-sidebar.tsx
@@ -27,7 +27,7 @@ import {
 import Image from "next/image";
 import useCan from "./auth/use-can";
 import { Permission } from "@/constants/permissions";
-import { useLocale } from "next-intl";
+import { useLocale, useTranslations } from "next-intl";
 
 type FilteredRoute = {
   name: string;
@@ -35,77 +35,60 @@ type FilteredRoute = {
   icon: LucideIcon;
   permission: Permission;
 };
-type user = {
-  name: string;
-  email: string;
-  avatar: string;
-};
-
-// This is sample data.
-
-const data: {
-  user: user;
-  routes: FilteredRoute[];
-} = {
-  user: {
-    name: "safwan",
-    email: "safwanmabdo@gmail.com",
-    avatar: "/avatars/shadcn.jpg",
-  },
-  routes: [
-    {
-      name: "Overview",
-      url: "/dashboard",
-      icon: Frame,
-      permission: "property.read",
-    },
-    {
-      name: "Properties",
-      url: "/dashboard/properties",
-      icon: House,
-      permission: "property.read",
-    },
-    {
-      name: "Create New Property",
-      url: "/dashboard/properties/create",
-      icon: HousePlus,
-      permission: "property.write",
-    },
-    {
-      name: "Blog Posts",
-      url: "/dashboard/blog-posts",
-      icon: LibraryBig,
-      permission: "blogpost.read",
-    },
-    {
-      name: "Create Blog Posts",
-      url: "/dashboard/blog-posts/create",
-      icon: NotebookPen,
-      permission: "blogpost.write",
-    },
-    {
-      name: "Users",
-      url: "/dashboard/users",
-      icon: Users,
-      permission: "user.read",
-    },
-    {
-      name: "Create Users",
-      url: "/dashboard/users/create",
-      icon: UserPlus,
-      permission: "user.write",
-    },
-  ],
-};
-
 
 export function DashboardSidebar({
   ...props
 }: React.ComponentProps<typeof Sidebar>) {
   const locale = useLocale();
+  const t = useTranslations("dashboard.sidebar")
   const {can} = useCan()
+
+  const routes: FilteredRoute[] = [
+    {
+      name: t("overview"),
+      url: "/dashboard",
+      icon: Frame,
+      permission: "property.read",
+    },
+    {
+      name: t("properties"),
+      url: "/dashboard/properties",
+      icon: House,
+      permission: "property.read",
+    },
+    {
+      name: t("createProperty"),
+      url: "/dashboard/properties/create",
+      icon: HousePlus,
+      permission: "property.write",
+    },
+    {
+      name: t("blogPosts"),
+      url: "/dashboard/blog-posts",
+      icon: LibraryBig,
+      permission: "blogpost.read",
+    },
+    {
+      name: t("createBlogPost"),
+      url: "/dashboard/blog-posts/create",
+      icon: NotebookPen,
+      permission: "blogpost.write",
+    },
+    {
+      name: t("users"),
+      url: "/dashboard/users",
+      icon: Users,
+      permission: "user.read",
+    },
+    {
+      name: t("createUser"),
+      url: "/dashboard/users/create",
+      icon: UserPlus,
+      permission: "user.write",
+    },
+  ]
   
-  const filteredRoutes = data.routes.filter((route) => {
+  const filteredRoutes = routes.filter((route) => {
     const canSee = can(route.permission);
     if (!canSee) return null;
     return route;

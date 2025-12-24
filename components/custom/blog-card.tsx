118b76d51ca28db34ef3672b3fc605ff4c667dee
sfwnisme <92028514+sfwnisme@users.noreply.github.com>
2025-12-25 00:01:56 +0300

    fix: replace Link -> NextIntlLink

--- components/custom/blog-card.tsx
+++ components/custom/blog-card.tsx
@@ -2,15 +2,14 @@ import { PAGES_ROUTES } from "@/constants/config";
 import { getBlogPostImage } from "@/lib/requests";
 import { BlogPost } from "@/types/types";
 import Image from "next/image";
-import Link from "next/link";
-import React from "react";
+import { Link as NextIntlLink } from "@/i18n/navigation";
 
 export default async function BlogCard({ blogPost }: { blogPost: BlogPost }) {
   const blogPostImage = await getBlogPostImage(blogPost?._id);
 
   const blogPostImageData = blogPostImage.data;
   return (
-    <Link
+    <NextIntlLink
       href={PAGES_ROUTES.BLOG_POSTS.PREVIEW + "/"+blogPost.slug}
       className="group/blogCard relative overflow-hidden rounded-2xl cursor-pointer size-full max-h-[300px]"
     >
@@ -24,6 +23,6 @@ export default async function BlogCard({ blogPost }: { blogPost: BlogPost }) {
       <div className="absolute bg-primary/20 left-0 bottom-0 xl:-bottom-full group-hover/blogCard:bottom-0 duration-500 group-hover/blogCard:duration-500 backdrop-blur-2xl w-full p-4 text-white">
         {blogPost.title}
       </div>
-    </Link>
+    </NextIntlLink>
   );
 }

a8a02eb0fbd8f907c10a102f02e7253ebcb29560
Moamal-2000 <moamalalaapro1@gmail.com>
2025-12-27 15:52:52 +0300

    update(request): wrap message import with try/catch and render not found page on error

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,15 +1,22 @@
+import { routing } from "@/i18n/routing";
 import { hasLocale } from "next-intl";
 import { getRequestConfig } from "next-intl/server";
-import { routing } from "@/i18n/routing";
+import { notFound } from "next/navigation";
 
-export default getRequestConfig(async ({requestLocale}) => {
+export default getRequestConfig(async ({ requestLocale }) => {
   const requested = await requestLocale;
+
   const locale = hasLocale(routing.locales, requested)
     ? requested
     : routing.defaultLocale;
 
-  return {
-    locale,
-    messages: (await import(`../messages/${locale}.json`)).default,
-  };
+  let messages;
+  try {
+    messages = (await import(`../messages/${locale}.json`)).default;
+  } catch (error) {
+    console.log("Import Message Error: ", error);
+    notFound();
+  }
+
+  return { locale, messages };
 });

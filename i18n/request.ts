13f0085cd1a123732ccbc35eed9621ee5fda3068
Moamal-2000 <moamalalaapro1@gmail.com>
2025-12-27 16:23:54 +0300

    Revert "update(request): wrap message import with try/catch and render not found page on error"
    This reverts commit a8a02eb0fbd8f907c10a102f02e7253ebcb29560.

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,22 +1,15 @@
-import { routing } from "@/i18n/routing";
 import { hasLocale } from "next-intl";
 import { getRequestConfig } from "next-intl/server";
-import { notFound } from "next/navigation";
+import { routing } from "@/i18n/routing";
 
-export default getRequestConfig(async ({ requestLocale }) => {
+export default getRequestConfig(async ({requestLocale}) => {
   const requested = await requestLocale;
-
   const locale = hasLocale(routing.locales, requested)
     ? requested
     : routing.defaultLocale;
 
-  let messages;
-  try {
-    messages = (await import(`../messages/${locale}.json`)).default;
-  } catch (error) {
-    console.log("Import Message Error: ", error);
-    notFound();
-  }
-
-  return { locale, messages };
+  return {
+    locale,
+    messages: (await import(`../messages/${locale}.json`)).default,
+  };
 });

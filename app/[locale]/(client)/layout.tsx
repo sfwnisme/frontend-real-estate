fabfde5e9edc05a91a32cb30def9304326d04b07
sfwnisme <92028514+sfwnisme@users.noreply.github.com>
2026-01-02 16:21:07 +0300

    fix: remove unused next-intl config

--- app/[locale]/(client)/layout.tsx
+++ app/[locale]/(client)/layout.tsx
@@ -1,11 +1,8 @@
 import FooterView from '@/features/client/footer-view'
-import NavView from '@/features/client/nav-view'
+  import NavView from '@/features/client/nav-view'
 import React from 'react'
-import { setRequestLocale } from 'next-intl/server'
 
-export default async  function layout({ children, params }: { children: React.ReactNode, params: Promise<{locale: string}> }) {
-  const locale = (await params).locale;
-  setRequestLocale(locale);
+export default async  function layout({ children }: { children: React.ReactNode }) {
   return (
     <div data-component="pages-layout" className='bg-gray-50'>
       <NavView />

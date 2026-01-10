091add9151904bab60c00b34ffb0b821cf3e65c2
sfwnisme <92028514+sfwnisme@users.noreply.github.com>
2026-01-10 21:02:11 +0300

    chore: translate /blog-posts user dir

--- app/[locale]/(client)/(pages)/blog-posts/page.tsx
+++ app/[locale]/(client)/(pages)/blog-posts/page.tsx
@@ -1,7 +1,7 @@
 import PaginationLayout from "@/components/custom/pagination-layout";
 import Title from "@/components/custom/title";
-import { SITE_INFO } from "@/constants/config";
-import { PAGINATION_CONFIG, STATUS_TEXT } from "@/constants/enums";
+import { PAGES_ROUTES } from "@/constants/config";
+import { PAGINATION_CONFIG } from "@/constants/enums";
 import BlogPostCardSkeleton from "@/features/blog-posts/skeletons/blog-post-card-skeleton";
 import BlogPostsGridView from "@/features/blog-posts/views/blog-posts-grid-view";
 import { getBlogPosts } from "@/lib/requests";
@@ -10,36 +10,66 @@ import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { Suspense } from "react";
 import { getTranslations } from "next-intl/server";
+import {
+  WEBSITE_URL,
+  WEBSITE_URL_EN,
+  WEBSITE_URL_AR,
+} from "@/constants/config";
+
+const { PREVIEW } = PAGES_ROUTES.BLOG_POSTS;
+const { PAGE } = PAGINATION_CONFIG.BLOG.CLIENT;
 
 export const generateMetadata = async ({
   searchParams,
+  params,
 }: {
   searchParams: SearchParamsType;
+  params: Promise<{ locale: string }>;
 }): Promise<Metadata> => {
+  const { locale } = await params;
+  const localeParam = locale === "en" ? "/en" : "";
+  const canonical = `${WEBSITE_URL}${localeParam}${PREVIEW}`;
+  const enCanonical = `${WEBSITE_URL_EN}${PREVIEW}`;
+  const arCanonical = `${WEBSITE_URL_AR}${PREVIEW}`;
+
   const page = (await searchParams)?.page;
   const currentPage = page ? parseInt(page) : 1;
-  const blogPosts = await getBlogPosts(
-    PAGINATION_CONFIG.BLOG.CLIENT.PAGE,
-    currentPage
-  );
-
+  const blogPosts = await getBlogPosts(PAGE, currentPage);
   if (!blogPosts.data) {
     return {};
   }
-
   const { nextPage, prevPage } = blogPosts.data;
-  const { TITLE, DESCRIPTION, ROUTE } = SITE_INFO.PAGES.BLOG;
+
+  const t = await getTranslations("Metadata.blogPosts");
+  const title = t("title");
+  const description = t("description");
+  const ogTitle = t("ogTitle");
+  const ogDescription = t("ogDescription");
+  const keywords = [title, ogTitle];
+  const next = canonical + `?page=${nextPage}`;
+  const previous = canonical + `?page=${prevPage}`;
 
   return {
-    title: TITLE,
-    description: DESCRIPTION,
+    title,
+    description,
     alternates: {
-      canonical: ROUTE,
+      canonical,
+      languages: {
+        "x-default": canonical,
+        en: enCanonical,
+        ar: arCanonical,
+      },
     },
     pagination: {
-      next: ROUTE + `?page=${nextPage}`,
-      previous: ROUTE + `?page=${prevPage}`,
+      next,
+      previous,
     },
+    openGraph: {
+      title: ogTitle,
+      description: ogDescription,
+      images: [{ url: WEBSITE_URL + "/hero-bg.webp" }],
+    },
+    keywords,
   };
 };
 

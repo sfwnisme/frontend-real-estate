517205c9a28e5b243bb13688ec9f6ee1a2598f7e
sfwnisme <92028514+sfwnisme@users.noreply.github.com>
2026-01-10 22:00:52 +0300

    fix: disallow indexing /login route

--- app/robots.ts
+++ app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: '/dashboard',
+      disallow: ['/dashboard', '/login'],
     },
     sitemap: `${BASE_URL}/sitemap.xml`,
   }

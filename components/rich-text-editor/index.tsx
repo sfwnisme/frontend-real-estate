6c5d489c87f5fc82691fad4557db3987252897a7
sfwnisme <92028514+sfwnisme@users.noreply.github.com>
2026-01-17 23:59:13 +0300

    chore: configure editorState instead of shouldRenderOnTransaction
    - configure Link extension
    
    - configure text directions RTL and LTR
    
    - configure Highlight extension

--- components/rich-text-editor/index.tsx
+++ components/rich-text-editor/index.tsx
@@ -5,12 +5,86 @@ import StarterKit from "@tiptap/starter-kit";
 import MenuBar from "./menu-bar";
 import TextAlign from "@tiptap/extension-text-align";
 import Image from "@tiptap/extension-image";
-import HorizontalRule from "@tiptap/extension-horizontal-rule";
 import Youtube from "@tiptap/extension-youtube";
+import Link from "@tiptap/extension-link";
+import Highlight from '@tiptap/extension-highlight'
 
 interface RichTextEditorProps {
   content?: string;
 }
+
+const LinkConfig = Link.configure({
+  HTMLAttributes: {
+    class: "text-blue-500 hover:text-blue-600 underline",
+  },
+  openOnClick: false,
+  autolink: true,
+  defaultProtocol: "https",
+  protocols: ["http", "https"],
+  isAllowedUri: (url: string, ctx: any) => {
+    try {
+      // construct URL
+      const parsedUrl = url.includes(":")
+        ? new URL(url)
+        : new URL(`${ctx.defaultProtocol}://${url}`);
+
+      // use default validation
+      if (!ctx.defaultValidate(parsedUrl.href)) {
+        return false;
+      }
+
+      // disallowed protocols
+      const disallowedProtocols = ["ftp", "file", "mailto"];
+      const protocol = parsedUrl.protocol.replace(":", "");
+
+      if (disallowedProtocols.includes(protocol)) {
+        return false;
+      }
+
+      // only allow protocols specified in ctx.protocols
+      const allowedProtocols = ctx.protocols.map((p: any) =>
+        typeof p === "string" ? p : p.scheme
+      );
+
+      if (!allowedProtocols.includes(protocol)) {
+        return false;
+      }
+
+      // disallowed domains
+      const disallowedDomains = ["example-phishing.com", "malicious-site.net"];
+      const domain = parsedUrl.hostname;
+
+      if (disallowedDomains.includes(domain)) {
+        return false;
+      }
+
+      // all checks have passed
+      return true;
+    } catch {
+      return false;
+    }
+  },
+  shouldAutoLink: (url) => {
+    try {
+      // construct URL
+      const parsedUrl = url.includes(":")
+        ? new URL(url)
+        : new URL(`https://${url}`);
+
+      // only auto-link if the domain is not in the disallowed list
+      const disallowedDomains = [
+        "example-no-autolink.com",
+        "another-no-autolink.com",
+      ];
+      const domain = parsedUrl.hostname;
+
+      return !disallowedDomains.includes(domain);
+    } catch {
+      return false;
+    }
+  },
+});
+
 export default function RichTextEditor({
   content = "<p>Hello World! 🌎️</p>",
 }: RichTextEditorProps) {
@@ -38,8 +112,12 @@ export default function RichTextEditor({
         width: 840,
         height: 480,
       }),
-      HorizontalRule,
+      LinkConfig,
+      Highlight.configure({
+        multicolor: true,
+      }),
     ],
+    textDirection: "auto",
     editorProps: {
       attributes: {
         class:
@@ -47,8 +125,8 @@ export default function RichTextEditor({
       },
     },
     content: content,
-    immediatelyRender: true,
-    shouldRerenderOnTransaction: true,
+    immediatelyRender: false,
+    // shouldRerenderOnTransaction: true,
   });
   return (
     <div className="m-8">
--- components/rich-text-editor/menu-bar.tsx
+++ components/rich-text-editor/menu-bar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Editor } from "@tiptap/react";
+import { Editor, useEditorState } from "@tiptap/react";
 import {
   AlignCenter,
   AlignLeft,
@@ -9,11 +9,15 @@ import {
   Heading1,
   Heading2,
   Heading3,
+  HighlighterIcon,
   Image,
   Italic,
+  Link,
   List,
   ListOrdered,
   Minus,
+  PilcrowLeft,
+  PilcrowRight,
   Quote,
   Redo,
   Strikethrough,
@@ -23,10 +27,14 @@ import {
 } from "lucide-react";
 import { Toggle } from "../ui/toggle";
 import { Tooltip, TooltipTrigger, TooltipContent } from "../ui/tooltip";
+import { useCallback } from "react";
 
 export default function MenuBar({ editor }: { editor: Editor | null }) {
   if (!editor) return null;
 
+  //---------------------
+  // actions
+  //---------------------
   const addImage = () => {
     const url = window.prompt("URL of the image");
     if (url) {
@@ -44,13 +52,78 @@ export default function MenuBar({ editor }: { editor: Editor | null }) {
       });
     }
   };
+  const setLink = useCallback(() => {
+    const previousUrl = editor.getAttributes("link").href;
+    const url = window.prompt("URL", previousUrl);
+
+    // cancelled
+    if (url === null) {
+      return;
+    }
+
+    // empty
+    if (url === "") {
+      editor.chain().focus().extendMarkRange("link").unsetLink().run();
+
+      return;
+    }
+
+    // update link
+    try {
+      editor
+        .chain()
+        .focus()
+        .extendMarkRange("link")
+        .setLink({ href: url })
+        .run();
+    } catch (e: any) {
+      alert(e.message);
+    }
+  }, [editor]);
+
+  const setHighlight = useCallback(() => {
+    editor.chain().focus().toggleHighlight({ color: "#ffc078" }).run();
+  }, [editor]);
+
+  //---------------------
+  // editor state
+  //---------------------
+  const editorState = useEditorState({
+    editor,
+    selector: (ctx) => ({
+      isHeading1: ctx.editor.isActive("heading", { level: 1 }),
+      isHeading2: ctx.editor.isActive("heading", { level: 2 }),
+      isHeading3: ctx.editor.isActive("heading", { level: 3 }),
+      isQuote: ctx.editor.isActive("blockquote"),
+      isBold: ctx.editor.isActive("bold"),
+      isItalic: ctx.editor.isActive("italic"),
+      isUnderline: ctx.editor.isActive("underline"),
+      isStrike: ctx.editor.isActive("strike"),
+      isAlignLeft: ctx.editor.isActive("textAlign", { align: "left" }),
+      isAlignCenter: ctx.editor.isActive("textAlign", { align: "center" }),
+      isAlignRight: ctx.editor.isActive("textAlign", { align: "right" }),
+      isLTR: ctx.editor.isActive("ltr"),
+      isRTL: ctx.editor.isActive("rtl"),
+      isBulletList: ctx.editor.isActive("bulletList"),
+      isOrderedList: ctx.editor.isActive("orderedList"),
+      isHorizontalRule: false,
+      isImage: false,
+      isYoutubeVideo: false,
+      isUndo: false,
+      isRedo: false,
+      isClearFormatting: false,
+      isLink: ctx.editor.isActive("link"),
+      isLinkActive: ctx.editor.isActive("link"),
+      isHighlight: ctx.editor.isActive("highlight"),
+    }),
+  });
 
   const menuItems = [
     {
       name: "heading1",
       icon: Heading1,
       action: () => editor?.chain().focus().toggleHeading({ level: 1 }).run(),
-      isActive: editor.isActive("heading", { level: 1 }),
+      isActive: editorState.isHeading1,
       canActivate:
         editor.can().chain().toggleHeading({ level: 1 }).run() ?? false,
     },
@@ -58,7 +131,7 @@ export default function MenuBar({ editor }: { editor: Editor | null }) {
       name: "heading2",
       icon: Heading2,
       action: () => editor?.chain().focus().toggleHeading({ level: 2 }).run(),
-      isActive: editor.isActive("heading", { level: 2 }),
+      isActive: editorState.isHeading2,
       canActivate:
         editor.can().chain().toggleHeading({ level: 2 }).run() ?? false,
     },
@@ -66,7 +139,7 @@ export default function MenuBar({ editor }: { editor: Editor | null }) {
       name: "heading3",
       icon: Heading3,
       action: () => editor?.chain().focus().toggleHeading({ level: 3 }).run(),
-      isActive: editor.isActive("heading", { level: 3 }),
+      isActive: editorState.isHeading3,
       canActivate:
         editor.can().chain().toggleHeading({ level: 3 }).run() ?? false,
     },
@@ -74,116 +147,143 @@ export default function MenuBar({ editor }: { editor: Editor | null }) {
       name: "quote",
       icon: Quote,
       action: () => editor?.chain().focus().toggleBlockquote().run(),
-      isActive: editor.isActive("blockquote"),
+      isActive: editorState.isQuote,
       canActivate: editor.can().chain().toggleBlockquote().run() ?? false,
     },
     {
       name: "bold",
       icon: Bold,
       action: () => editor?.chain().focus().toggleBold().run(),
-      isActive: editor.isActive("bold"),
+      isActive: editorState.isBold,
       canActivate: editor.can().chain().toggleBold().run() ?? false,
     },
     {
       name: "italic",
       icon: Italic,
       action: () => editor?.chain().focus().toggleItalic().run(),
-      isActive: editor.isActive("italic"),
+      isActive: editorState.isItalic,
       canActivate: editor.can().chain().toggleItalic().run() ?? false,
     },
     {
       name: "underline",
       icon: Underline,
       action: () => editor?.chain().focus().toggleUnderline().run(),
-      isActive: editor.isActive("underline"),
+      isActive: editorState.isUnderline,
       canActivate: editor.can().chain().toggleUnderline().run() ?? false,
     },
     {
       name: "strike",
       icon: Strikethrough,
       action: () => editor?.chain().focus().toggleStrike().run(),
-      isActive: editor.isActive("strike"),
+      isActive: editorState.isStrike,
       canActivate: editor.can().chain().toggleStrike().run() ?? false,
+    },
+    {
+      name: "highlight",
+      icon: HighlighterIcon,
+      action: () => setHighlight(),
+      isActive: editorState.isHighlight,
+      canActivate: editor.can().chain().toggleHighlight().run() ?? false,
     },
     {
       name: "align left",
       icon: AlignLeft,
       action: () => editor?.chain().focus().setTextAlign("left").run(),
-      isActive: editor.isActive("textAlign", { align: "left" }),
+      isActive: editorState.isAlignLeft,
       canActivate: editor.can().chain().setTextAlign("left").run() ?? false,
     },
     {
       name: "align center",
       icon: AlignCenter,
       action: () => editor?.chain().focus().setTextAlign("center").run(),
-      isActive: editor.isActive("textAlign", { align: "center" }),
+      isActive: editorState.isAlignCenter,
       canActivate: editor.can().chain().setTextAlign("center").run() ?? false,
     },
     {
       name: "align right",
       icon: AlignRight,
       action: () => editor?.chain().focus().setTextAlign("right").run(),
-      isActive: editor.isActive("textAlign", { align: "right" }),
+      isActive: editorState.isAlignRight,
       canActivate: editor.can().chain().setTextAlign("right").run() ?? false,
+    },
+    {
+      name: "text direction LTR",
+      icon: PilcrowRight,
+      action: () => editor.commands.setTextDirection("ltr"),
+      isActive: editorState.isLTR,
+      canActivate: editor.can().chain().setTextDirection("ltr").run() ?? false,
+    },
+    {
+      name: "text direction RTL",
+      icon: PilcrowLeft,
+      action: () => editor.commands.setTextDirection("rtl"),
+      isActive: editorState.isRTL,
+      canActivate: editor.can().chain().setTextDirection("rtl").run() ?? false,
     },
     {
       name: "bullet list",
       icon: List,
       action: () => editor?.chain().focus().toggleBulletList().run(),
-      isActive: editor.isActive("bulletList"),
+      isActive: editorState.isBulletList,
       canActivate: editor.can().chain().toggleBulletList().run() ?? false,
     },
     {
       name: "ordered list",
       icon: ListOrdered,
       action: () => editor?.chain().focus().toggleOrderedList().run(),
-      isActive: editor.isActive("orderedList"),
+      isActive: editorState.isOrderedList,
       canActivate: editor.can().chain().toggleOrderedList().run() ?? false,
     },
     {
       name: "horizontal rule",
       icon: Minus,
       action: () => editor?.chain().focus().setHorizontalRule().run(),
-      isActive: editor.isActive("horizontalRule"),
+      isActive: editorState.isHorizontalRule,
       canActivate: editor.can().chain().setHorizontalRule().run() ?? false,
+    },
+    {
+      name: "link",
+      icon: Link,
+      action: () => setLink(),
+      isActive: editorState.isLink,
+      canActivate: true,
     },
     {
       name: "image",
       icon: Image,
       action: () => addImage(),
-      isActive: editor.isActive("image"),
+      isActive: editorState.isImage,
       canActivate: true,
     },
     {
       name: "youtube video",
       icon: YoutubeIcon,
       action: () => addYoutubeVideo(),
-      isActive: editor.isActive("youtubeVideo"),
+      isActive: editorState.isYoutubeVideo,
       canActivate: true,
     },
     {
       name: "undo",
       icon: Undo,
       action: () => editor?.chain().focus().undo().run(),
-      isActive: false,
+      isActive: editorState.isUndo,
       canActivate: editor.can().chain().undo().run() ?? false,
     },
     {
       name: "redo",
       icon: Redo,
       action: () => editor?.chain().focus().redo().run(),
-      isActive: false,
+      isActive: editorState.isRedo,
       canActivate: editor.can().chain().redo().run() ?? false,
     },
     {
       name: "clear formatting",
       icon: Eraser,
       action: () => editor?.chain().focus().clearNodes().run(),
-      isActive: false,
+      isActive: editorState.isClearFormatting,
       canActivate: editor.can().chain().clearNodes().run() ?? false,
     },
   ];
-  console.log("isActive", editor.isActive("bold"));
 
   const renderMenuItems = menuItems.map(({ icon: IconComponent, ...item }) => (
     <Tooltip key={item.name}>
@@ -196,6 +296,7 @@ export default function MenuBar({ editor }: { editor: Editor | null }) {
           onPressedChange={item.action}
           disabled={!item.canActivate}
           size="default"
+          variant="outline"
         >
           <IconComponent className="size-4" />
         </Toggle>

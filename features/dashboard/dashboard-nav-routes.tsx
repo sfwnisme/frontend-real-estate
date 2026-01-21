1a97440cbb9bbc7808a8a62fe17d677e02c84ace
sfwnisme <92028514+sfwnisme@users.noreply.github.com>
2026-01-21 22:02:47 +0300

    chore: format code

--- features/dashboard/dashboard-nav-routes.tsx
+++ features/dashboard/dashboard-nav-routes.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { type LucideIcon } from "lucide-react";
-
 import {
   SidebarGroup,
   SidebarMenu,
@@ -9,9 +8,8 @@ import {
   SidebarMenuItem,
   useSidebar,
 } from "@/components/ui/sidebar";
-import Link from "next/link";
 import { Permission } from "@/constants/permissions";
-import {Link as NextIntlLink} from "@/i18n/navigation"
+import { Link as NextIntlLink } from "@/i18n/navigation";
 
 export function DashboardNavRoutes({
   projects,
@@ -23,19 +21,18 @@ export function DashboardNavRoutes({
     permission: Permission;
   }[];
 }) {
-  const { isMobile } = useSidebar();
   return (
     <SidebarGroup className="group-data-[collapsible=icon]:hidden">
       <SidebarMenu>
         {projects.map((item) => (
-            <SidebarMenuItem key={item.name}>
-              <SidebarMenuButton asChild>
-                <NextIntlLink href={item.url}>
-                  <item.icon />
-                  <span>{item.name}</span>
-                </NextIntlLink>
-              </SidebarMenuButton>
-            </SidebarMenuItem>
+          <SidebarMenuItem key={item.name}>
+            <SidebarMenuButton asChild>
+              <NextIntlLink href={item.url}>
+                <item.icon />
+                <span>{item.name}</span>
+              </NextIntlLink>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
         ))}
       </SidebarMenu>
     </SidebarGroup>

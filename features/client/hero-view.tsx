2ac46f92bad0b1ac2315f1f9b8a667a403ca34fe
sfwnisme <92028514+sfwnisme@users.noreply.github.com>
2026-01-23 10:24:58 +0300

    fix: use semantic html to title and description and proper translation source

--- features/client/hero-view.tsx
+++ features/client/hero-view.tsx
@@ -1,34 +1,32 @@
-import React from "react";
 import { ArrowDown } from "lucide-react";
-import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-} from "../../components/ui/avatar";
 import { Button } from "../../components/ui/button";
 import { useTranslations } from "next-intl";
+import { Typography } from "@/components/custom/typography";
 
 type Props = {};
 
 export default function Hero({}: Props) {
-  const t = useTranslations("HomePage")
-  console.log(t("title"))
+  const t = useTranslations("SiteConfig");
+  const tButton = useTranslations("HomePage");
+  console.log(t("title"));
   return (
-      <header
-        className="relative h-[calc(100dvh-100px)] box-border bg-center bg-cover bg-no-repeat bg-gray-100 rounded-4xl"
-        style={{ backgroundImage: "url('/hero-bg.webp')" }}
+    <header
+      className="relative h-[calc(100dvh-100px)] box-border bg-center bg-cover bg-no-repeat bg-gray-100 rounded-4xl"
+      style={{ backgroundImage: "url('/hero-bg.webp')" }}
+    >
+      <Button
+        variant="outline"
+        size="lg"
+        className="absolute bg-transparent backdrop-blur-sm bottom-4 left-1/2 -translate-x-1/2 text-white"
       >
-        <Button
-          variant="outline"
-          size="lg"
-          className="absolute bg-transparent backdrop-blur-sm bottom-4 left-1/2 -translate-x-1/2 text-white"
-        >
-          {t("button")} <ArrowDown />
-        </Button>
-        <div className="flex flex-col items-center gap-4 bg-gray-100 rounded-3xl py-8 px-6 absolute md:bottom-4 md:top-auto top-1/2 md:right-4 right-1/2 md:translate-y-0 -translate-y-1/2 md:translate-x-0 translate-x-1/2 text-center">
-          <h1 className="text-7xl font-medium">70+</h1>
-          <p className="text-sm w-40">{t("description")}</p>
-        </div>
-      </header>
+        {tButton("button")} <ArrowDown />
+      </Button>
+      <div className="flex flex-col items-center gap-10 bg-gray-100 rounded-3xl py-8 px-6 absolute md:bottom-4 md:top-auto top-1/2 md:right-4 right-1/2 md:translate-y-0 -translate-y-1/2 md:translate-x-0 translate-x-1/2 text-center">
+        <Typography variant="h1" as="p">{t("name")}</Typography>
+        <Typography variant="muted" as="h1" className="w-40">
+          {t("description")}
+        </Typography>
+      </div>
+    </header>
   );
 }

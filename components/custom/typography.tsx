7f6f5c11e94028de4780e9b73b1228c473234f53
sfwnisme <92028514+sfwnisme@users.noreply.github.com>
2026-01-23 10:13:36 +0300

    refactor: change text weight and line-hight

--- components/custom/typography.tsx
+++ components/custom/typography.tsx
@@ -6,7 +6,7 @@ import { cn } from "@/lib/utils";
 const typographyVariants = cva("", {
   variants: {
     variant: {
-      h1: "scroll-m-20 text-2xl font-extrabold tracking-tight sm:text-3xl md:text-4xl lg:text-5xl",
+      h1: "scroll-m-20 text-2xl font-bold tracking-tight sm:text-3xl md:text-4xl lg:text-5xl",
       h2: "scroll-m-20 text-xl font-semibold tracking-tight sm:text-2xl md:text-3xl",
       h3: "scroll-m-20 text-lg font-semibold tracking-tight sm:text-xl md:text-2xl",
       h4: "scroll-m-20 text-base font-semibold tracking-tight sm:text-lg md:text-xl",
@@ -15,7 +15,7 @@ const typographyVariants = cva("", {
       p: "text-sm leading-7 sm:text-base [&:not(:first-child)]:mt-4 sm:[&:not(:first-child)]:mt-6",
       lead: "text-base text-muted-foreground sm:text-lg md:text-xl",
       large: "text-base font-semibold sm:text-lg",
-      small: "text-xs font-medium leading-none sm:text-sm",
+      small: "text-xs sm:text-sm",
       muted: "text-xs text-muted-foreground sm:text-sm",
       blockquote: "mt-4 border-l-2 pl-4 italic sm:mt-6 sm:pl-6",
     },

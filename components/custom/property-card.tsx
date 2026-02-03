90ee6815d79435ee02c31b773c537263acd3ee04
sfwnisme <92028514+sfwnisme@users.noreply.github.com>
2026-02-03 22:19:42 +0300

    refactor(property-card): restyle the button position

--- components/custom/property-card.tsx
+++ components/custom/property-card.tsx
@@ -32,22 +32,34 @@ export default async function PropertyCard({
           />
         )}
       </div>
-      <div className="absolute bottom-0 left-1/2 -translate-x-1/2 p-4 w-full text-white font-medium backdrop-blur-2xl bg-transparent group-hover:bg-white max-xl:bg-white group-hover:bottom-5 max-xl:bottom-5 group-hover:rounded-2xl max-xl:rounded-2xl group-hover:w-[90%] max-xl:w-[90%] transition-all group-hover:transition-all duration-500 group-hover:duration-500 flex items-center">
+      <div className="absolute bottom-0 left-1/2 -translate-x-1/2 p-4 w-full text-white font-medium backdrop-blur-2xl bg-transparent group-hover:bg-white max-xl:bg-white group-hover:bottom-5 max-xl:bottom-5 group-hover:rounded-2xl max-xl:rounded-2xl group-hover:w-[90%] max-xl:w-[90%] transition-all group-hover:transition-all duration-300 group-hover:duration-300 flex items-center">
         <div className="flex-1">
           <p className="text-sm group-hover:text-gray-500 max-xl:text-gray-500 pb-1">
             {property.title}
           </p>
-          <span className="text-xl text-gray-700 font-semibold hidden group-hover:block max-xl:block">
-            {Number(property.price).toLocaleString("en-SA", {style: "currency", currency: "SAR"})}
-          </span>
+          <div className="inline-flex gap-2 items-center justify-between w-full">
+            <span className="text-xl text-gray-700 font-semibold hidden group-hover:block max-xl:block">
+              {Number(property.price).toLocaleString("en-SA", {
+                style: "currency",
+                currency: "SAR",
+              })}
+            </span>
+            <div className="block xl:hidden group-hover:block text-black">
+              <ButtonLink
+                href={PAGES_ROUTES.PROPERTIES.PREVIEW + "/" + property.slug}
+              >
+                {tActions("viewAll")}
+              </ButtonLink>
+            </div>
+          </div>
         </div>
-        <div className="block xl:hidden group-hover:block text-black">
+        {/*<div className="block xl:hidden group-hover:block text-black">
           <ButtonLink
             href={PAGES_ROUTES.PROPERTIES.PREVIEW + "/" + property.slug}
           >
             {tActions("viewAll")}
           </ButtonLink>
-        </div>
+        </div>*/}
       </div>
     </div>
   );

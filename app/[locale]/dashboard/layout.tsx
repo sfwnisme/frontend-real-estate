3a07695fcdc173ecf4d455e08a2ab72a4439812e
sfwnisme <92028514+sfwnisme@users.noreply.github.com>
2026-03-17 04:51:56 +0300

    chore(dashboard): apply theme mode to dashboard only

--- app/[locale]/dashboard/layout.tsx
+++ app/[locale]/dashboard/layout.tsx
@@ -7,45 +7,57 @@ import dynamic from "next/dynamic";
 import { redirect } from "next/navigation";
 import React, { Suspense } from "react";
 import { getTranslations } from "next-intl/server";
+import { ThemeProvider } from "@/components/theme-provider";
 const GlobalDeleteDialog = dynamic(
   () => import("@/features/dashboard/global-delete-dialog"),
   {
     loading: () => <LoadingPage />,
-  }
+  },
 );
 
 type Props = {
   children: React.ReactNode;
 };
 
-export async function generateMetadata({ params }: { params: Promise<{ locale: string }> }): Promise<Metadata> {
-  const locale = (await params).locale
-  const t = await getTranslations("SiteConfig")
-  const DASHBOARD_TITLE = t("dashboard.title")
-  const DASHBOARD_DESCRIPTION = t("dashboard.description")
-  const SITE_NAME = t("name")
-  const PAGE_TITLE = `${SITE_NAME} - ${DASHBOARD_TITLE}`
-  const PAGE_TEMPLATE_AR = `${PAGE_TITLE} | %s`
-  const PAGE_TEMPLATE_EN = `%s | ${PAGE_TITLE}`
-  const PAGE_TEMPLATE = locale === "ar" ? PAGE_TEMPLATE_AR : PAGE_TEMPLATE_EN
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}): Promise<Metadata> {
+  const locale = (await params).locale;
+  const t = await getTranslations("SiteConfig");
+  const DASHBOARD_TITLE = t("dashboard.title");
+  const DASHBOARD_DESCRIPTION = t("dashboard.description");
+  const SITE_NAME = t("name");
+  const PAGE_TITLE = `${SITE_NAME} - ${DASHBOARD_TITLE}`;
+  const PAGE_TEMPLATE_AR = `${PAGE_TITLE} | %s`;
+  const PAGE_TEMPLATE_EN = `%s | ${PAGE_TITLE}`;
+  const PAGE_TEMPLATE = locale === "ar" ? PAGE_TEMPLATE_AR : PAGE_TEMPLATE_EN;
   return {
     title: {
       template: PAGE_TEMPLATE,
       default: PAGE_TITLE,
     },
     description: DASHBOARD_DESCRIPTION,
-  }
+  };
 }
 
 export default async function layout({ children }: Props) {
-  const currentUser = await getCurrentUser()
-  if(!currentUser.data) {
+  const currentUser = await getCurrentUser();
+  if (!currentUser.data) {
     redirect("/login");
   }
   return (
     <div className="relative">
       <AuthProvider user={currentUser?.data}>
-        <DashboardLayout>{children}</DashboardLayout>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <DashboardLayout>{children}</DashboardLayout>
+        </ThemeProvider>
       </AuthProvider>
       <Suspense fallback={null}>
         <GlobalDeleteDialog />

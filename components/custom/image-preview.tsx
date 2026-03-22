53086bb27673260a7d868a3a3e4e6ba3d389a573
sfwnisme <92028514+sfwnisme@users.noreply.github.com>
2026-03-23 02:24:18 +0300

    chore(image-preview): new props, aspectRatio, hideInfo, and className

--- components/custom/image-preview.tsx
+++ components/custom/image-preview.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Image from "next/image";
 import { cn, returnFileSize } from "@/lib/utils";
 import { X } from "lucide-react";
@@ -9,9 +9,9 @@ import LoadingSpinner from "./loading-spinner";
 import { Typography } from "./typography";
 
 type Props = {
+  imageUrl: string;
   imageSize: number;
   imageType: string;
-  imageUrl: string;
   isFeatured?: boolean;
   disableSetFeaturedImage?: boolean;
   deleteImage?: () => void;
@@ -20,6 +20,9 @@ type Props = {
   setFeaturedImage?: () => void;
   isLoading?: boolean;
   error?: string | null;
+  aspectRatio?: "video" | "square" | "auto";
+  hideInfo?: boolean;
+  className?: string;
 };
 
 const ImageError = (props: {
@@ -30,7 +33,12 @@ const ImageError = (props: {
   return (
     <div className="flex flex-col gap-2 items-center justify-center text-center size-full absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 bg-primary/70 p-2">
       {<small className="text-xs text-destructive">{error}</small>}
-      <Button variant="destructive" size="sm" onClick={deleteImage} type="button">
+      <Button
+        variant="destructive"
+        size="sm"
+        onClick={deleteImage}
+        type="button"
+      >
         Remove
       </Button>
     </div>
@@ -38,64 +46,88 @@ const ImageError = (props: {
 };
 
 const ImageLoading = () => {
-  return <div className="size-full absolute grid place-items-center"><LoadingSpinner /></div>
-}
+  return (
+    <div className="size-full absolute grid place-items-center">
+      <LoadingSpinner />
+    </div>
+  );
+};
 
 export default function ImagePreview(props: Props) {
+  const {
+    imageSize = 0,
+    imageType = "",
+    imageUrl = "",
+    isFeatured = false,
+    disableSetFeaturedImage = false,
+    deleteImage = () => {},
+    disableDeleteImage = false,
+    hideDeleteImage = false,
+    setFeaturedImage = () => {},
+    isLoading = false,
+    error = null,
+    aspectRatio = "square",
+    hideInfo = false,
+    className = "",
+  } = props;
+
+  const aspectRatioClass = aspectRatio === "video" ? "aspect-video" : aspectRatio === "square"? "aspect-square" : "aspect-auto";
   return (
     <div
       className={cn(
-        "group flex flex-col relative aspect-video rounded-md overflow-hidden border",
+        "group flex flex-col relative rounded-md overflow-hidden border",
+        aspectRatioClass,
+        className,
       )}
-      data-featured={props.isFeatured}
-      data-error={!!props.error}
+      data-featured={isFeatured}
+      data-error={!!error}
     >
-      {props.isLoading && <ImageLoading />}
-      {props.error && (
+      {isLoading && <ImageLoading />}
+      {error && (
         <ImageError
-          error={props.error}
-          deleteImage={props.deleteImage ?? (() => {})}
+          error={error}
+          deleteImage={deleteImage ?? (() => {})}
         />
       )}
       <Image
-        src={props.imageUrl}
+        src={imageUrl}
         width={"300"}
         height={"300"}
-        alt={props.imageType}
+        alt={imageType}
         className="object-contain size-full"
       />
       <div className="flex flex-col items-end justify-end gap-1 p-2 absolute bottom-0 left-0 size-full bg-linear-to-t from-black/40 to-transparent text-black">
         <div className="flex gap-1 items-center justify-between w-full">
-          <Typography as="small" size="xxs" className="text-white">
-            {returnFileSize(props.imageSize)},{" "}
-            {props.imageType.split("/")[1].toUpperCase()}
+          <Typography as="small" size="xxs" className="text-white" hidden={hideInfo}>
+            {returnFileSize(imageSize)},{" "}
+            {imageType.split("/")[1].toUpperCase()}
           </Typography>
-          {!props.disableSetFeaturedImage && (
+          {!disableSetFeaturedImage && (
             <Label
               className={cn("flex items-center gap-1")}
               title="Set as main image"
             >
               <Checkbox
-                id={props.imageUrl + "featured"}
+                id={imageUrl + "featured"}
                 className={cn(
                   "data-[state=checked]:border-yellow-500 data-[state=checked]:bg-yellow-500 data-[state=checked]:text-white dark:data-[state=checked]:border-yellow-500 dark:data-[state=checked]:bg-yellow-500 disabled:cursor-not-allowed!",
                 )}
-                checked={props.isFeatured}
-                onCheckedChange={props.setFeaturedImage}
-                disabled={props.disableSetFeaturedImage}
+                checked={isFeatured}
+                onCheckedChange={setFeaturedImage}
+                disabled={disableSetFeaturedImage}
               />
             </Label>
           )}
         </div>
       </div>
-      {!props.hideDeleteImage && (
+      {!hideDeleteImage && (
         <div className="z-10 absolute top-2 right-2 flex flex-col gap-2 items-center justify-center group-data-[error=true]:hidden">
           <div className={cn("flex items-center justify-between gap-2")}>
             <Button
               variant="secondary"
               size="icon"
-              onClick={props.deleteImage}
-              disabled={props.disableDeleteImage}
+              onClick={deleteImage}
+              disabled={disableDeleteImage}
               className="rounded-full size-6"
               aria-label="delete image"
               title="Delete image"

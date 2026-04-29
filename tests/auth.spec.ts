9617ced531fbbe88259d0ca5d38eb57683240811
sfwnisme <safymo424@gmail.com>
2026-04-30 02:43:34 +0300

    chore(tests): config clear baseURL, use locale helper for routes, and remove Promise

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -1,24 +1,24 @@
 import { PAGES_ROUTES } from "@/constants/config";
 import { test, expect } from "@playwright/test";
+import { locale } from "./helpers";
 /** Chunk 1.1 — Login page renders (Day 1, ~1h)
  * Navigate to /login and verify the form is visible: email field, password field, submit button.
  * Verify page title / main heading is correct.
  */
 
+const { E2E_EMAIL, E2E_PASS } = process.env;
+
 test.describe("Authentication", () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto("login");
+    await page.goto(locale("/login"));
   });
   test("scan login page", async ({ page }) => {
     await expect(page).toHaveTitle("Login | Real Estate");
   });
 
   test("scan login form", async ({ page }) => {
-    await expect(
-      page.getByTestId("login-form"), 
-      "form element"
-    ).toBeVisible()
-    
+    await expect(page.getByTestId("login-form"), "form element").toBeVisible();
+
     await expect(
       page.locator("[type=email][name=email]"),
       "email input",
@@ -28,17 +28,15 @@ test.describe("Authentication", () => {
       "password input",
     ).toBeVisible();
     await expect(
-      page.getByRole("button", {name: /login/i}), "login input"
-    ).toBeVisible()
+      page.getByRole("button", { name: /login/i }),
+      "login input",
+    ).toBeVisible();
   });
 
-  test("act login", async({page}) => {
-    await page.locator("[type=email][name=email]").fill("admin@admin.admin")
-    await page.locator("[type=password][name=password]").fill("admin")
-    await Promise.all([
-      page.getByRole("button", { name: /login/i }).click(),
-      page.waitForURL(`**/${PAGES_ROUTES.DASHBOARD}`),
-    ]);
-    await expect(page, "redirect to dashboard").toHaveURL("dashboard");
-  })
+  test("act login and redirect to dashboard", async ({ page }) => {
+    await page.locator("[type=email][name=email]").fill(E2E_EMAIL!);
+    await page.locator("[type=password][name=password]").fill(E2E_PASS!);
+    await page.getByRole("button", { name: /login/i }).click();
+    await expect(page, "redirect to dashboard").toHaveURL(locale(PAGES_ROUTES.DASHBOARD));
+  });
 });
